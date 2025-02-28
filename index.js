89b2f922d2adc36d310f571c00ac9ff494b7bb75
quark-gluon-plasma <huntermaloysmith@gmail.com>
2025-02-28 14:30:16 -0500

    Create index.js
    added color test functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function RGB(){
+  document.getElementbyId("p1").style.color = "red";
+  document.getElementbyId("p2").style.color = "green";
+  document.getElementbyId("p3").style.color = "blue";
+}
+function CMYK(){
+  document.getElementbyId("p1").style.color = "cyan";
+  document.getElementbyId("p2").style.color = "magenta";
+  document.getElementbyId("p3").style.color = "yellow";
+}
